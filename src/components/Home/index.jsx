f1168ac2f38db0e7d7abba6f953c83abd88da2ac
Volkov <mark.volkovgit@gmail.com>
2021-10-23 13:56:09 +0300

    useMemo && useCallbeck

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,10 +1,11 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState, useCallback, useMemo } from 'react'
 import { ThemeContaxt } from '../../contexts/'
 import cx from 'classnames'
 import constants from '../../constants'
 import style from './Home.module.sass'
 
 const Home = () => {
+    const [value, setValue] = useState(1)
     const { THEMES: { LIGHT, DARK } } = constants
     const [theme, setTheme] = useContext(ThemeContaxt)
 
@@ -13,8 +14,25 @@ const Home = () => {
         [style.dark]: theme === DARK
     })
 
+
+    const preCount = useMemo(() => {
+        let i = 0;
+        for (let j = 0; j < 1000000000; j++) {
+            i += j;
+        }
+        return i;
+    }, []);
+    const count = useCallback(value => preCount ** value, [value])
+    const currentValue = useMemo(() => count(value), [value]);
+    // console.log(preCount)
+
     return (
-        <div className={classes}>Home page</div>
+        <div className={classes}>
+            <h1>Home page</h1>
+            <div>value : {value}</div>
+            <div>{currentValue}</div>
+            <button onClick={() => setValue((prev => prev + 1))}>+</button>
+        </div>
     )
 }
 
