4efd87be05a41291e2718b3f6d2fc47454efc328
Volkov <mark.volkovgit@gmail.com>
2021-10-26 08:28:20 +0300

    add Formik from example

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
+import SignInForm from 'components/forms/SignInForm';
 import React from 'react'
-import { SIGN_UP_SCHEMA } from 'utils/validationSchemas'
+
 function App() {
-  const data = SIGN_UP_SCHEMA.validateSync({ password: 'dsfDF321#$', email: 'serfentos12@mail.ru', lastName: 'Test', firstName: 'Testovich' })
   return (
     <>
-      <div>{data.password}</div>
+      <SignInForm />
     </>
   )
 }
--- src/components/forms/SignInForm/index.jsx
+++ src/components/forms/SignInForm/index.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
+import { Formik, Form, Field, ErrorMessage } from 'formik'
+import { SIGN_IN_SCHEMA } from 'utils/validationSchemas'
+
+const initialValues = {
+    email: '',
+    password: '',
+}
+
 
 function SignInForm() {
     return (
-        <div>
-
-        </div>
+        <Formik initialValues={initialValues} validationSchema={SIGN_IN_SCHEMA}>
+            {fomikProps => {
+                console.log(fomikProps)
+                return (
+                    <Form>
+                        <Field name='email' />
+                        <ErrorMessage name='email' component='span' />
+                        <Field type="password" name='password' />
+                        <ErrorMessage name='password' />
+                        <Field type="submit" value='Submit' />
+                    </Form>
+                )
+            }}
+        </Formik>
     )
 }
 
